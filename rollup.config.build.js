d81c38b62cd82d7dd0c7c8b67588d9d3fddb6b14
Kelin2025 <kelin2025@yandex.ru>
2017-11-24 03:33:41 +0300

    Fixed build configs

--- rollup.config.build.js
+++ rollup.config.build.js
@@ -5,11 +5,18 @@ import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: 'src/index.js',
-  output: {
-    name: 'nanoclone',
-    file: 'dist/index.js',
-    format: 'es'
-  },
+  output: [
+    {
+      name: 'nanoclone',
+      file: 'dist/index.js',
+      format: 'umd'
+    },
+    {
+      name: 'nanoclone',
+      file: 'dist/index.esm.js',
+      format: 'es'
+    }
+  ],
   plugins: [
     babel({
       exclude: 'node_modules/**'
