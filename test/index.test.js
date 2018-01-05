5fcc9105396ab9488b9b4348d17f6f63ccbe833b
Kelin2025 <kelin2025@yandex.ru>
2018-01-05 23:44:20 +0300

    Improved tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const { JSDOM } = require('jsdom')
 
 const nanoclone = require('../index')
 
-it('Null/undefined/functions/etc', () => {
+it('Returns equal data for Null/undefined/functions/etc', () => {
   // Null
   expect(nanoclone(null)).toBeNull()
 
@@ -20,7 +20,7 @@ it('Null/undefined/functions/etc', () => {
   expect(nanoclone(true)).toBe(true)
 })
 
-it('DOM Node', () => {
+it('Returns equal data for DOM Node', () => {
   const src = new JSDOM(`<p>Hello world</p>`).window.document
 
   const copy = nanoclone(src)
@@ -28,34 +28,35 @@ it('DOM Node', () => {
   expect(src.body.isEqualNode(copy.body)).toBe(true)
 })
 
-it('Date', () => {
+it('Returns equal data for Date', () => {
   const date = '2012-01-26T13:51:50.417Z'
 
   expect(nanoclone(new Date(date))).toEqual(new Date(date))
 })
 
-it('RegExp', () => {
+it('Returns equal data for RegExp', () => {
   const regexp = /^$/
 
   expect(nanoclone(regexp)).toEqual(regexp)
 })
 
-it('Arrays', () => {
+it('Returns equal data for Arrays', () => {
   const tests = [
     [5, 5, 8, 'string'], // Flat
     [5, 5, 8, { a: 'string' }, [7, 9]] // Attached
   ]
 
-  tests.forEach((src) => {
+  tests.forEach(src => {
     const copy = nanoclone(src)
 
     expect(src).toEqual(copy)
   })
 })
 
-it('Object', () => {
+it('Returns equal data for Object', () => {
   const src = {
-    a: 5, b: 6
+    a: 5,
+    b: 6
   }
 
   const copy = nanoclone(src)
@@ -63,7 +64,7 @@ it('Object', () => {
   expect(src).toEqual(copy)
 })
 
-it('Map', () => {
+it('Returns equal data for Map', () => {
   const src = new Map([['foo', 'bar']])
 
   const copy = nanoclone(src)
@@ -71,10 +72,80 @@ it('Map', () => {
   expect(src).toEqual(copy)
 })
 
-it('Set', () => {
+it('Returns equal data for Set', () => {
   const src = new Set(['foo', 'bar'])
 
   const copy = nanoclone(src)
 
   expect(src).toEqual(copy)
+})
+
+test("Doesn't clone function", () => {
+  const src = function b () {}
+
+  const copy = nanoclone(src)
+
+  expect(copy).toBe(src)
+})
+
+test('Clones Date object', () => {
+  var src = new Date()
+
+  var copy = nanoclone(src)
+
+  copy.setHours(src.getHours() + 1) // +1 hour
+
+  expect(copy.getHours()).not.toBe(src.getHours())
+})
+
+test('Clones RegExp', () => {
+  var src = new RegExp()
+
+  var copy = nanoclone(src)
+
+  expect(copy).not.toBe(src)
+})
+
+test('Clones Array with nested data', () => {
+  var src = [1, 'hello', [null, 'lalka']]
+
+  var copy = nanoclone(src)
+
+  copy[2][0] = 'mutated'
+  expect(src[2][0]).toBeNull()
+
+  copy = copy.map(i => 'mutated')
+
+  expect(src.every(i => i !== 'mutated')).toBeTruthy()
+})
+
+test('Clones Object with nested data', () => {
+  var src = { a: 1, b: { c: 1, d: [1, 2, 3] } }
+  var srcValues = { a: 1, b: { c: 1, d: [1, 2, 3] } }
+
+  var copy = nanoclone(src)
+  copy.a = 2
+  copy.b.c = 'asdf'
+  copy.b.d[1] = 4
+  expect(src).toEqual(srcValues)
+})
+
+it('Clones Map', () => {
+  const src = new Map([['foo', 'bar']])
+
+  const copy = nanoclone(src)
+
+  copy.set('foo', 'baz')
+
+  expect(src.get('foo')).toEqual('bar')
+})
+
+it('Clones Set', () => {
+  const src = new Set(['foo', 'bar'])
+
+  const copy = nanoclone(src)
+
+  copy.add('baz')
+
+  expect(src.has('baz')).toBeFalsy()
 })
